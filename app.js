1174e89f2d0b078b4fa387f2f66693a3ae3d5d19
mveron007 <mveron13814@gmail.com>
2024-01-31 23:56:03 -0300

    adding express && handlebars

--- app.js
+++ app.js
@@ -1,8 +1,18 @@
 import express from 'express';
+import expressHandlebars from 'express-handlebars';
+import http from 'http';
+import { Server } from 'socket.io';
 import cartsRouter from './routes/carts.router.js';
 import productsRouter from './routes/products.router.js';
+
 const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
 const PORT = 8080;
+
+
+app.engine('handlebars', expressHandlebars());
+app.set('view engine', 'handlebars');
 
 app.use(express.json());
 app.use('/api/products', productsRouter);
